9718b2cfac89184500afa13cf3054add9ff5bab4
helzheng123 <helen.zheng@stonybrook.edu>
2023-11-21 04:12:24 +0000

    2nd upload

--- Part2/flask1/app.py
+++ Part2/flask1/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello from Flask App 1 !!!!'
+def home():
+    messages = [ "Welcome to my Flask App!", 
+                "欢迎来到我的Flask应用!",
+                "私のFlaskアプリへようこそ!", 
+                "ยินดีต้อนรับสู่แอป Flask ของฉัน!", 
+                "내 Flask 앱에 오신 것을 환영합니다!",
+                "Bienvenue dans mon application Flask!",
+                "Willkommen in meiner Flask-Anwendung!"]
+    random_message = random.choice(messages)
+    return render_template('index.html', random_message=random_message)
+
+@app.route('/about')
+def about():
+    return 'This is a simple Flask application with random messages in different languages.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- Part2/flask2/app.py
+++ Part2/flask2/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+#in Celsius
+
 @app.route('/')
-def index():
-    return 'Hello from Flask App 1 !!!!'
+def home():
+    cities = [
+        {"name": "New York", "temperature": random.randint(0, 30), "condition": "Sunny"},
+        {"name": "Paris", "temperature": random.randint(10, 25), "condition": "Cloudy"},
+        {"name": "Tokyo", "temperature": random.randint(15, 30), "condition": "Rainy"},
+        {"name": "Sydney", "temperature": random.randint(20, 35), "condition": "Clear"},
+        {"name": "Moscow", "temperature": random.randint(-10, 10), "condition": "Snowy"},
+    ]
+
+    random_city = random.choice(cities)
+    return render_template('about.html', city=random_city)
+
+@app.route('/about')
+def about():
+    return 'This is a simple Flask application with different countries and random weather information.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
